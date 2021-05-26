494c84becd322c542157c3cec15820e7d3da7c72
Zubair Matani <64571329+mataniz@users.noreply.github.com>
2021-05-25 21:40:19 -0400

    Add files via upload

---
+++ script_1.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/sh
+
+pwd
+clear
+date
+df
+du
+less
+find okc.txt; #pwd; less
+pw#d
+who
+touch okc.txt; ls;# who; du; less
+# Ignore this line 
+# THis one too!
+#pwd
+dff
+
+dda; dhd; sj; pwd
+
+exit
